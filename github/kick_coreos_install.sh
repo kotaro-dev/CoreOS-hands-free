5339d0de7d99e6029317dc2f19e49412e2f44425
sasaki <sasaki@backyard.com>
2014-12-08 16:49:57 +0900

    change http svr from github to local

--- github/kick_coreos_install.sh
+++ github/kick_coreos_install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-dhcphost=raw.github.com/kotaro-dev/CoreOS-hands-free/github/
-branch=master
-subpath=github
+# dhcphost=192.168.131.10
+# branch=master
+# subpath=github
 
-sudo wget http://${dhcphost}${branch}/${subpath}/cloud-config.yml -O /home/core/cloud-config.yml
+sudo wget http://192.168.131.10/cloud-config.yml -O /home/core/cloud-config.yml
 
-sudo coreos-install -d /dev/sda -C stable -c cloud-config.yml
+sudo coreos-install -d /dev/sda -C stable -c /home/core/cloud-config.yml
 
 sudo eject
 
