84fd6060330bca2e3743e2d5258cc58f66c37f60
sasaki <sasaki@backyard.com>
2014-12-09 17:00:19 +0900

    change directory and add change hostname code

---
+++ http-svr/kick_coreos_install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+httpsvr=192.168.131.10
+# branch=master
+# subpath=github
+
+sudo wget http://${httpsvr}/cloud-config.yml -O /home/core/cloud-config.yml
+
+#change hostname based ip guest addr
+bshstnm=gstcore
+gstip=`ip route|grep enp0s8|cut -f 12 -d " "|cut -f 4 -d .`
+gstnm=${bshstnm}${gstip}
+
+echo ${gstnm}
+
+##sed -e s/coreos-handoff/${gstnm]/g /home/core/cloud-config.yml
+sudo sed "/^hostname: /c\hostname: ${gstnm}" /home/core/cloud-config.yml > /home/core/cloud-config
+
+if [ "${bshstnm}" == "${gstnm}" ]; then
+  sudo coreos-install -d /dev/sda -C stable -c /home/core/cloud-config.yml
+else
+  sudo coreos-install -d /dev/sda -C stable -c /home/core/cloud-config
+fi
+
+sudo eject
+
+sudo reboot
